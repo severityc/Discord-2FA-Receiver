1039ceaff59d318ffff645462e47c8aa42553ccd
Severity <158026132+severityc@users.noreply.github.com>
2024-04-03 23:34:15 -0700

    🔥
    updated code🔥

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pyotp
 import os
+import time
+import sys
+from colorama import Fore, Style
 
-os.system('cls')
-key = input("Enter 2FA Key (Manual Entry) : ")
+os.system('clear')
 
-totp = pyotp.totp(key)
+def get_current_code(key):
+    totp = pyotp.TOTP(key)
+    return totp.now()
 
-print("\n  > Current code: " + totp.now())
+def read_key_from_file(filename):
+    with open(filename, 'r') as file:
+        return file.read().strip()
+    
+key = read_key_from_file('key.txt')
+print(f"{Fore.LIGHTGREEN_EX}[TOTP]{Fore.RESET} Initial code : " + get_current_code(key))
 
-
+try:
+    while True:
+        current_code = get_current_code(key)
+        print(f"{Fore.LIGHTGREEN_EX}[TOTP]{Fore.RESET} Refreshed code : " + current_code)
+        time.sleep(1)  
+except KeyboardInterrupt:
+    print("\nExiting...")
+    sys.exit(0)
